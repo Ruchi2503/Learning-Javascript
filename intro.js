bd95678fc49519872c0ea9cbf2056b2186695987
Ruchi2503 <152387455+Ruchi2503@users.noreply.github.com>
2025-08-03 13:19:45 +0000

    Add intro.js and remove unnecessary files

---
+++ intro.js
@@ -0,0 +1,23 @@
+console.log("Let's code") 
+
+// In JavaScript, console.log is used to print/output messages to the browser's console
+//  you may use this semicolon u may not ; its totally depend on u, this will not give u any error
+
+console.log("welcome to colmplete javascript notes");
+
+console.log('23+56')
+
+// javascript is a programming lanugage
+
+
+
+
+
+
+
+
+
+
+
+
+
