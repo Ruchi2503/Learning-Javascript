dfa64621ef5797c19b81b24da09675005c261383
Ruchi2503 <152387455+Ruchi2503@users.noreply.github.com>
2025-08-05 04:32:36 +0000

    Logical operator

---
+++ Logical_operator.js
@@ -0,0 +1,26 @@
+// Logical operator
+// And  (&&)
+// Or   (||)
+// Not  (!=)
+
+// && operator means when both condition should true then only it will print
+const age=20;
+const gender='male';
+if(age>=18 && gender=='female'){
+    console.log('you are adult male')
+}
+
+
+//  || operator means atleast one condtion sholud be true
+// if(age>=18 || gender=='female'){ in this only condition is true then also it will run the code
+
+
+if(age%2==0){
+    consloe.log("even")
+}
+else if(age %2==0){
+    console.log("odd")
+}
+else{
+    console.log("Nothing")
+}
